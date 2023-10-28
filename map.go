dfd4fa14e51c3bee8e16f0b5362cb625208d892a
hooks_28 <1r56955415918@qq.com>
2023-10-28 11:34:13 +0800

    feat: 初始化package并迁移代码，仓库为可用状态

---
+++ map.go
@@ -0,0 +1,55 @@
+package memoMap
+
+import (
+	"errors"
+	"fmt"
+	"github.com/memo28-space-golang/memo28.slice"
+)
+
+type MapKeys interface {
+	~bool | ~int | ~int16 | ~int8 | ~int64 | ~float32 | ~float64 | ~string | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type Map[T MapKeys, V any] map[T]V
+
+// Keys 参考js Object.keys
+func (receiver Map[T, V]) Keys() memoSlice.Slice[T] {
+
+	keys := make(memoSlice.Slice[T], 0, len(receiver))
+	for k := range receiver {
+		keys.Push(k)
+	}
+	return keys
+}
+
+// Values 参考js Object.values
+func (receiver Map[T, V]) Values() *memoSlice.Slice[V] {
+	keys := make(memoSlice.Slice[V], 0, len(receiver))
+	for _, val := range receiver {
+		keys.Push(val)
+	}
+	return &keys
+}
+
+func (receiver Map[T, V]) Has(k T) bool {
+	_, ok := receiver[k]
+	return ok
+}
+
+func (receiver Map[T, V]) Get(k T) (V, error) {
+	value, ok := receiver[k]
+	if ok != false {
+		return value, nil
+	}
+	return value, errors.New(fmt.Sprint(k, "non-existent"))
+}
+
+func (receiver Map[T, V]) Add(key T, val V) Map[T, V] {
+	receiver[key] = val
+	return receiver
+}
+
+func (receiver Map[T, V]) del(key T) Map[T, V] {
+	delete(receiver, key)
+	return receiver
+}
---
+++ map_test.go
@@ -0,0 +1,81 @@
+package memoMap
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestKeys(t *testing.T) {
+	assert := assert.New(t)
+	result := Map[int, int]{
+		1: 1,
+		2: 2,
+		3: 444,
+	}
+
+	val := 2
+	include := result.Keys().Include(val)
+	assert.EqualValues(
+		true,
+		include, fmt.Sprint("keys 有误", include),
+	)
+}
+
+func TestValues(t *testing.T) {
+	assertions := assert.New(t)
+	result := Map[int, int]{
+		1: 1,
+		2: 2,
+		3: 444,
+	}
+	val := 444
+
+	include := result.Values().Include(val)
+	assertions.EqualValues(
+		true,
+		include,
+		fmt.Sprint("values error", include),
+	)
+}
+
+func TestHas(t *testing.T) {
+	assertions := assert.New(t)
+	result := Map[int, int]{
+		1: 1,
+		2: 2,
+		3: 444,
+	}
+
+	assertions.EqualValues(false, result.Has(10), "has 10 不存在")
+}
+
+func TestGet(t *testing.T) {
+	assertions := assert.New(t)
+	result := Map[int, int]{
+		1: 1,
+		2: 2,
+		3: 444,
+	}
+	value, _ := result.Get(1)
+	assertions.EqualValues(1, value, fmt.Sprint("key 1 === ", value))
+}
+
+func TestAdd(t *testing.T) {
+	assertions := assert.New(t)
+
+	m := Map[string, int]{}
+	get, _ := m.Add("200", 200).Add("300", 300).Get("200")
+
+	assertions.EqualValues(200, get, fmt.Sprint("add value ====", get))
+}
+
+func TestDel(t *testing.T) {
+	assertions := assert.New(t)
+
+	m := Map[string, int]{}
+
+	has := m.Add("200", 200).del("200").Has("200")
+
+	assertions.EqualValues(false, has, fmt.Sprint("del value ====", has))
+}
